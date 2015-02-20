fe15769be0af84dfad5b4d6b9a15a8eb6af6998a
David Fono <fono@mobilefono.com>
2015-02-20 11:57:04 -0500

    trying caching

--- src/js/view/LineView.js
+++ src/js/view/LineView.js
@@ -16,10 +16,10 @@ define(function(require) {
         this.frame.graphics.drawRoundRect(0, 0, width, height, 5, 10);
         this.frame.setBounds(0, 0, width, height);
 
-        this.cache(0, 0, width, height);
-
         this.addChild(this.frame);
         this.addChild(this.text);
+
+        this.cache(0, 0, width, height);
     };
     LineView.prototype = Object.create(createjs.Container.prototype);
     LineView.prototype.constructor = LineView;
