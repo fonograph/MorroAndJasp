3504565508c6986caa87664435cf9f90d7aec701
David Fono <fono@mobilefono.com>
2016-03-28 14:06:41 -0400

    app cache updates

--- src/js/state/PreloadState.js
+++ src/js/state/PreloadState.js
@@ -20,26 +20,34 @@ define(function(require) {
         
         if ( appCache ) {
             appCache.addEventListener('progress', function(e){
-                console.log(e);
                 number.text = Math.round((e.loaded/e.total)*100) + '%';
             }.bind(this));
             appCache.addEventListener('cached', function(e){
+                console.log('Cache complete.');
                 this.end();
             }.bind(this));
             appCache.addEventListener('updateready', function(e){
+                console.log('Update found.');
                 appCache.swapCache();
                 this.end();
             }.bind(this));
             appCache.addEventListener('noupdate', function(e){
+                console.log('Nothing to download.');
                 this.end();
             }.bind(this));
             appCache.addEventListener('error', function(e){
+                console.log('App cache error.');
                 this.end();
             }.bind(this));
 
             window.setTimeout(function() {
                 if ( appCache.status == appCache.IDLE ) {
                     console.log('Nothing to download.');
+                    this.end();
+                }
+                else if ( appCache.status == appCache.UPDATEREADY ) {
+                    console.log('Update found.');
+                    appCache.swapCache();
                     this.end();
                 }
             }.bind(this), 500);
@@ -54,7 +62,10 @@ define(function(require) {
     PreloadState.prototype.constructor = PreloadState;
 
     PreloadState.prototype.end = function(){
-        game.setState('title');
+        if ( !this.ended ) {
+            this.ended = true;
+            game.setState('title');
+        }
     };
 
     createjs.promote(PreloadState, "super");
