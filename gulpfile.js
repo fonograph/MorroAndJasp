3197b40b6ace31f2018dfa89d763a70a2d238a12
David Fono <fono@mobilefono.com>
2016-03-27 23:04:45 -0400

    dont load wavs

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ gulp.task('audio', function(){
 });
 
 gulp.task('preload-manifest', function(){
-    return gulp.src(['www/assets/**/*.*'])
+    return gulp.src(['www/assets/**/*.*', '!**/*.wav', '!www/assets/fonts/**/*.*'])
         //.pipe(assetManifest({bundleName: 'assets', includeRelativePath: true, manifestFile: 'www/assets/preload-manifest.json'}))
         .pipe(manifest({
             hash: true,
