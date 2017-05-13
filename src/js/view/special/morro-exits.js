d72e0208e8fd5651befd04a90ba036783cda272e
David Fono <fono@mobilefono.com>
2017-05-13 08:17:40 -0400

    exits have length

--- src/js/view/special/morro-exits.js
+++ src/js/view/special/morro-exits.js
@@ -1,11 +1,18 @@
 define(function(require){
+    var Signal = require('signals').Signal;
 
     return function(sceneView){
+
+        this.signalOnComplete = new Signal();
 
         var direction = sceneView.morro.x < game.width/2 ? 1 : -1;
 
         TweenMax.to(sceneView.morro, 2.5, {x:'-='+(game.width*0.45*direction), ease:Power1.easeIn});
         TweenMax.to(sceneView.morro, 1.5, {y:'+=50', repeat:1, yoyo:true, ease:Power1.easeInOut});
+
+        TweenMax.delayedCall(2, function(){
+            this.signalOnComplete.dispatch();
+        }.bind(this));
 
     };
 
