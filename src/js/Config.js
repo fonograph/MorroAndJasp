4e381a452724ddf1f6412479d5ab81947c40a806
David Fono <fono@mobilefono.com>
2017-05-14 10:03:52 -0400

    use sad music for tragedy

--- src/js/Config.js
+++ src/js/Config.js
@@ -304,7 +304,7 @@ define(function(){
                 continues: true
             },
             tragedy: {
-                music: "epic"
+                music: "sad"
             },
             horror: {
                 music: "spooky",
