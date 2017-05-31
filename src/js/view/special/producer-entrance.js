fbc7527b6041235c01235840cc0681d46ef95491
David Fono <fono@mobilefono.com>
2017-05-31 08:36:36 -0400

    specials

---
+++ src/js/view/special/producer-entrance.js
@@ -0,0 +1,13 @@
+define(function(require) {
+    var Signal = require('signals').Signal;
+
+    return function (sceneView) {
+
+        this.signalOnComplete = new Signal();
+
+        sceneView.sound.playSound('door-open');
+        sceneView.sound.playSound('footsteps', 0, 0.7, function(){
+            this.signalOnComplete.dispatch();
+        }.bind(this));
+    };
+});
